f9a6537e038f99ccba84b7a48ef549362bac1d4d
victoriabolivar1411-lang <victoriabolivar1411@gmail.com>
2026-04-25 09:57:12 -0400

    base template done

--- base_project/settings.py
+++ base_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'base_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
