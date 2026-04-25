4f0fc64901b0acabf0b87dc3bd662015f6a73fb5
victoriabolivar1411-lang <victoriabolivar1411@gmail.com>
2026-04-25 11:37:25 -0400

    article detail done

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
-]       
+    path ("articulos/<int:pk>/", ArticleDetailView.as_view(), name='article-detail')
+    ]       
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 #Creatre your views here.
 class ArticleListView(ListView):
     model = Article
     template_name = 'articles.html'
+    context_object_name = 'article_list'
     
-    
+class ArticleDetailView(DetailView):
+        model = Article
+        template_name = 'article-detail.html'
+        context_object_name = 'article'
+        
+        
